7ca7e27d00c3f6b99e5d3180a15e84b8a0437f36
Fathimah Az-Zahra Suriawinata <fathimah.azzahra.tif425@polban.ac.id>
2026-02-24 05:43:28 +0700

    Update taskmanager.py

--- fathimah's/taskmanager.py
+++ fathimah's/taskmanager.py
@@ -8,5 +8,19 @@ def nomor_tugas():
     """Beri nomor untuk setiap tugas"""
     if not tasks:
         return 1
-    return max(task["id"] for task in tasks) + 1
+    return max(task["nomor tugas"] for task in tasks) + 1
 
+
+def tambah_tugas(title, deadline, priority):
+    """Menambahkan tugas baru"""
+    tugas_baru = {
+        "Nomor Tugas":nomor_tugas(),
+        "Title": title,
+        "Deadline": deadline,
+        "Priority (easy, medium, hard)": priority,
+        "Status": "Belum"
+    }
+
+    tasks.append(tugas_baru)
+    save_tasks(tasks)
+    return tugas_baru
