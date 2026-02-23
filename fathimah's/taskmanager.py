377dc753182d4ffcfda2925fabaf1eb4ccb7fde4
Fathimah Az-Zahra Suriawinata <fathimah.azzahra.tif425@polban.ac.id>
2026-02-24 06:08:09 +0700

    Update taskmanager.py

--- fathimah's/taskmanager.py
+++ fathimah's/taskmanager.py
@@ -4,7 +4,7 @@ from database import load_tasks, save_tasks
 
 tasks = load_tasks()
 
-def nomor_tugas():
+def no_tugas():
     """Beri nomor untuk setiap tugas"""
     if not tasks:
         return 1
@@ -14,7 +14,7 @@ def nomor_tugas():
 def tambah_tugas(title, deadline, priority):
     """Menambahkan tugas baru"""
     tugas_baru = {
-        "Nomor Tugas":nomor_tugas(),
+        "Nomor Tugas":no_tugas(),
         "Title": title,
         "Deadline": deadline,
         "Priority (easy, medium, hard)": priority,
